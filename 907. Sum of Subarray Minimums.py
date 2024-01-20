9a29fef530eadf9d2cc620ef6ec1eee812a55978
jannat-349 <jucse28.349@gmail.com>
2024-01-20 23:30:22 +0600

    medium

--- 907. Sum of Subarray Minimums.py
+++ 907. Sum of Subarray Minimums.py
@@ -1,14 +1,25 @@
 class Solution(object):
+
     def sumSubarrayMins(self, arr):
         """
         :type arr: List[int]
         :rtype: int
         """
         sum = 0
+        MOD = 10**9+7
+        stack = []
         n = len(arr)
 
-        for start in range(n):
-            for end in range(start + 1, n + 1):
-                sorted_subList = sorted(arr[start:end])
-                sum += sorted_subList[0]
-        return sum % 1000000007
+        for i in range(n+1):
+            while stack and (i == n or arr[stack[-1]] >= arr[i]):
+                mid = stack.pop()
+                left = stack[-1] if stack else -1
+                right = i
+                count = (mid-left) * (right-mid) % MOD
+                sum += (count * arr[mid]) % MOD
+                sum %= MOD
+            stack.append(i)
+
+        return sum
+
+        
