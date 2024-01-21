8ac72767269e0f8cae03e0590a696940d5f13b31
jannat-349 <jucse28.349@gmail.com>
2024-01-21 18:16:15 +0600

    medium used DP

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -4,14 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        rob = 0
-        norob = 0
+        n = len(nums)
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0], nums[1])
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        dp[2] = nums[0] + nums[2]
 
-        for num in nums:
-            rob2 = norob + num
-            norob2 = max(rob, norob)
-            rob = rob2
-            norob = norob2
+        for i in range(3, n):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-3]+nums[i])
         
-        return max(rob, norob)
-        
+        return max(dp)
