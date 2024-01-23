30b963df80e2d645a11934cb665dbefd40edc4cd
jannat-349 <jucse28.349@gmail.com>
2024-01-23 20:14:24 +0600

    medium using recursion

--- 1239. Maximum Length of a Concatenated String with Unique Characters.py
+++ 1239. Maximum Length of a Concatenated String with Unique Characters.py
@@ -4,24 +4,31 @@ class Solution(object):
         :type arr: List[str]
         :rtype: int
         """
-        dp = [0]
-        maxLen = 0
-        for word in arr:
-            a, dup = 0, 0
-            for alphabet in word:
-                dup |= a & (1 << (ord(alphabet) - ord('a')))
-                a |= 1 << (ord(alphabet) - ord('a'))
+        def getUniqueStringLen(index, visited):
+            if index >= len(arr):
+                return 0
             
-            if dup > 0:
-                continue
-            
-            for i in range(len(dp) - 1, -1, -1):
-                if (dp[i] & a) > 0:
-                    continue
-                dp.append(dp[i] | a)
-                maxLen = max(maxLen, bin(dp[i] | a).count('1'))
-        
-        return maxLen
-                    
+            maxLen = 0
+            isPossible = True
+            duplicate = set()
 
+            for alphabet in arr[index]:
+                if alphabet in visited or alphabet in duplicate:
+                    isPossible = False
+                    break
+                duplicate.add(alphabet)
+                
+            add, noAdd = 0, 0
+            if isPossible:
+                for alphabet in arr[index]:
+                    visited.add(alphabet)
+                add = len(arr[index]) + getUniqueStringLen(index+1, visited)
+                for alphabet in arr[index]:
+                    visited.remove(alphabet)
+            noAdd = getUniqueStringLen(index+1, visited)
+            maxLen += max(add, noAdd)
+            return maxLen
         
+        visited = set()
+        return getUniqueStringLen(0, visited)
+                    
