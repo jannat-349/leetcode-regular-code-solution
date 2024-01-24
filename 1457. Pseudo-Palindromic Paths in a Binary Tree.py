d42226f2dd1c8244497c3d1212980a60e43d1bb3
jannat-349 <jucse28.349@gmail.com>
2024-01-24 23:00:43 +0600

    medium using stack and bitwise operation

---
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pseudoPalindromicPaths (self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        numOfPalindromes = 0;
+        stack = [(root, 0)]
+
+        while stack:
+            node, path = stack.pop()
+
+            if node:
+                path ^= 1 << node.val
+
+                if not node.left and not node.right:
+                    if path & (path - 1) == 0:
+                        numOfPalindromes += 1
+                
+                else:
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+        
+        return numOfPalindromes
+        
