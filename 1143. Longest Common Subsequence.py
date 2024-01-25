3beeeb93a11c135555c6a6a0f7f2d5e5fce621a5
jannat-349 <jucse28.349@gmail.com>
2024-01-25 20:49:12 +0600

    medium LCS using memoization

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -5,14 +5,20 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
-
-        def lcs(x, y, m, n):
+        def lcs(x, y, m, n, memo):
             if m == 0 or n == 0:
                 return 0
+            elif memo[m][n] != -1:
+                return memo[m][n]
             elif x[m-1] == y[n-1]:
-                return 1 + lcs(x, y, m-1, n-1)
+                memo[m][n] = 1 + lcs(x, y, m-1, n-1, memo)
+                return memo[m][n]
             else:
-                return max(lcs(x, y, m, n-1), lcs(x, y, m-1, n))
+                memo[m][n] = max(lcs(x, y, m, n-1, memo), lcs(x, y, m-1, n, memo))
+                return memo[m][n]
         
-        return lcs(text1, text2, len(text1), len(text2))
+        m, n = len(text1), len(text2)
+        memo = [[-1] * (n + 1) for _ in range(m + 1)]
+
+        return lcs(text1, text2, m, n, memo)
         
