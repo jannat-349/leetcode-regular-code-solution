521fc2d0b0a1e3d938cce3c53247184e180aaecc
jannat-349 <jucse28.349@gmail.com>
2024-02-06 20:53:46 +0600

    medium using dictionary

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,23 +1,12 @@
 class Solution(object):
-    def getSign(self, word):
-        freq = Counter(word)
-        res = []
-        for i in range(26):
-            alphabet = chr(i + ord('a'))
-            if freq[alphabet] != 0:
-                res.extend([alphabet, str(freq[alphabet])])
- 
-        return ''.join(res)
-
     def groupAnagrams(self, strs):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        res = []
-        group = defaultdict(list)
+        res = {}
+
         for word in strs:
-            group[self.getSign(word)].append(word)
+            res.setdefault(''.join(sorted(word)), []).append(word)
         
-        res.extend(group.values())
-        return res
+        return res.values()
