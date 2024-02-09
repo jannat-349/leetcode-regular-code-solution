14e56bd770cad703501583ce409e028088403736
jannat-349 <jucse28.349@gmail.com>
2024-02-09 09:38:27 +0600

    medium using DP

---
+++ 368. Largest Divisible Subset.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        n = len(nums)
+        dp = [1] * n
+        max_size, max_index = 1, 0
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    if dp[i] > max_size:
+                        max_size += 1
+                        max_index = i
+        
+        answer = []
+        num = nums[max_index]
+        for i in range(max_index, -1, -1):
+            if num % nums[i] == 0 and dp[i] == max_size:
+                answer.append(nums[i])
+                num = nums[i]
+                max_size -= 1
+        
+
+        return answer
+
+        
